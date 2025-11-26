f36ced15e681ef4a31c0aa8ce2bbd0cba9ff5873
Harish T <harishachar9090@gmail.com>
2025-11-26 23:45:04 +0530

    Update database configuration for production

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,10 @@
 class Config:
     SECRET_KEY = 'your-secret-key-123-change-in-production'
     
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '12345' 
-    MYSQL_DB = 'chat_app'
+    MYSQL_HOST = 'sql3.freesqldatabase.com'
+    MYSQL_USER = 'sql3809573'
+    MYSQL_PASSWORD = 'bFEecYYM5f'  
+    MYSQL_DB = 'sql3809573'
     MYSQL_PORT = 3306
 
 config = Config()
